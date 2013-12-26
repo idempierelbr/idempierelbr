cc715b1605482df16fa67b8e02a3d93509c39a13
Alan Lescano <alan.lesc1@gmail.com>
2013-12-26 15:37:57 -0200

    Adicionando registro de scripts de migração

--- org.idempierelbr.core/migration/i2.0/oracle/201312261125.sql
+++ org.idempierelbr.core/migration/i2.0/oracle/201312261125.sql
@@ -14,3 +14,5 @@ UPDATE AD_Language SET IsLoginLocale='N',Updated=TO_DATE('2013-12-26 11:54:53','
 UPDATE AD_Language SET IsSystemLanguage='Y', IsLoginLocale='Y',Updated=TO_DATE('2013-12-26 11:28:27','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Language_ID=174
 ;
 
+SELECT lbr_register_migration_script('201312261125.sql') FROM dual
+;
