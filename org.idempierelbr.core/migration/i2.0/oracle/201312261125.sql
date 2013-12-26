ef17f283ac5e61ef10d268932cd1f1754827bb64
Alan Lescano <alan.lesc1@gmail.com>
2013-12-26 11:57:08 -0200

    Remove Espanhol(Colômbia) do Login

--- org.idempierelbr.core/migration/i2.0/oracle/201312261125.sql
+++ org.idempierelbr.core/migration/i2.0/oracle/201312261125.sql
@@ -6,6 +6,10 @@ SET DEFINE OFF
 UPDATE AD_Language SET IsSystemLanguage='N',Updated=TO_DATE('2013-12-26 11:26:41','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Language_ID=136
 ;
 
+-- Dec 26, 2013 11:54:53 AM BRST
+UPDATE AD_Language SET IsLoginLocale='N',Updated=TO_DATE('2013-12-26 11:54:53','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Language_ID=136
+;
+
 -- Dec 26, 2013 11:28:27 AM BRST
 UPDATE AD_Language SET IsSystemLanguage='Y', IsLoginLocale='Y',Updated=TO_DATE('2013-12-26 11:28:27','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Language_ID=174
 ;
--- org.idempierelbr.core/migration/i2.0/postgresql/201312261125.sql
+++ org.idempierelbr.core/migration/i2.0/postgresql/201312261125.sql
@@ -3,6 +3,10 @@
 UPDATE AD_Language SET IsSystemLanguage='N',Updated=TO_TIMESTAMP('2013-12-26 11:26:41','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Language_ID=136
 ;
 
+-- Dec 26, 2013 11:54:53 AM BRST
+UPDATE AD_Language SET IsLoginLocale='N',Updated=TO_TIMESTAMP('2013-12-26 11:54:53','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Language_ID=136
+;
+
 -- Dec 26, 2013 11:28:27 AM BRST
 UPDATE AD_Language SET IsSystemLanguage='Y', IsLoginLocale='Y',Updated=TO_TIMESTAMP('2013-12-26 11:28:27','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Language_ID=174
 ;
