c82a5ea3bf43379a15dfc06d294508429598f5bd
Alan Lescano <alan.lesc1@gmail.com>
2014-01-04 12:20:50 -0200

    Alteração do encoding para UTF-8

--- org.idempierelbr.core/migration/i2.0/oracle/201401040946.sql
+++ org.idempierelbr.core/migration/i2.0/oracle/201401040946.sql
@@ -2,7 +2,7 @@ SET SQLBLANKLINES ON
 SET DEFINE OFF
 
 -- 04/01/2014 9h27min32s BRST
--- Ajuste na visualização dos campos para quem utiliza LBR_USE_UNIFIED_BP e criação do registro de configuração
+-- Ajuste na visualizaÃ§Ã£o dos campos para quem utiliza LBR_USE_UNIFIED_BP e criaÃ§Ã£o do registro de configuraÃ§Ã£o
 UPDATE AD_Field SET DisplayLogic='@LBR_BPTypeBR@=''PJ'' & @#LBR_USE_UNIFIED_BP@!''Y''',Updated=TO_DATE('2014-01-04 09:27:32','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Field_ID=1000008
 ;
 
@@ -15,7 +15,7 @@ UPDATE AD_Field SET DisplayLogic='@LBR_BPTypeBR@=''PJ'' & @#LBR_USE_UNIFIED_BP@!
 ;
 
 -- 04/01/2014 10h1min40s BRST
-INSERT INTO AD_SysConfig (AD_SysConfig_ID,ConfigurationLevel,Value,Description,AD_SysConfig_UU,Created,Updated,AD_Org_ID,CreatedBy,IsActive,UpdatedBy,Name,AD_Client_ID,EntityType) VALUES (1000000,'C','N','Permite cadastrar filiais na aba Localização (PN) ao invés de criar um novo PN','694bcf92-692a-4f69-8623-729a7e7592b1',TO_DATE('2014-01-04 10:01:40','YYYY-MM-DD HH24:MI:SS'),TO_DATE('2014-01-04 10:01:40','YYYY-MM-DD HH24:MI:SS'),0,0,'Y',0,'LBR_USE_UNIFIED_BP',0,'LBR')
+INSERT INTO AD_SysConfig (AD_SysConfig_ID,ConfigurationLevel,Value,Description,AD_SysConfig_UU,Created,Updated,AD_Org_ID,CreatedBy,IsActive,UpdatedBy,Name,AD_Client_ID,EntityType) VALUES (1000000,'C','N','Permite cadastrar filiais na aba LocalizaÃ§Ã£o (PN) ao invÃ©s de criar um novo PN','694bcf92-692a-4f69-8623-729a7e7592b1',TO_DATE('2014-01-04 10:01:40','YYYY-MM-DD HH24:MI:SS'),TO_DATE('2014-01-04 10:01:40','YYYY-MM-DD HH24:MI:SS'),0,0,'Y',0,'LBR_USE_UNIFIED_BP',0,'LBR')
 ;
 
 SELECT lbr_register_migration_script('201401040946.sql') FROM dual
