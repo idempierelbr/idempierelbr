8a23c5ade931b0ffbe775aebc7336e266451ff45
Alan Lescano <alan.lesc1@gmail.com>
2014-03-12 14:06:20 -0300

    Preparando para a utilização do EventHandler

---
+++ org.idempierelbr.tax/src/org/idempierelbr/tax/model/EventHandler.java
@@ -0,0 +1,19 @@
+package org.idempierelbr.tax.model;
+
+import org.adempiere.base.event.AbstractEventHandler;
+import org.compiere.util.CLogger;
+import org.osgi.service.event.Event;
+
+public class EventHandler extends AbstractEventHandler {
+	/**	Logger			*/
+	private static CLogger log = CLogger.getCLogger(EventHandler.class);
+
+	@Override
+	protected void initialize() {
+	}
+
+	@Override
+	protected void doHandleEvent(Event event) {
+		// TODO Auto-generated method stub
+	}	
+}
