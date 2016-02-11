ab7db0c7aa7dad5387ec2e4c982ca8adf98110fc
Alan Lescano <alan.lesc1@gmail.com>
2016-02-11 09:43:13 -0200

    Issue #85 - Definição de Impostos em transações de compra

--- org.idempierelbr.tax/src/org/idempierelbr/tax/model/MLBRTax.java
+++ org.idempierelbr.tax/src/org/idempierelbr/tax/model/MLBRTax.java
@@ -920,8 +920,12 @@ public class MLBRTax extends X_LBR_Tax
 		}
 		
 		//	Tax Definition
+		int orgRegionID = oiLocation.getC_Location_ID() < 1 ? -1 : oiLocation.getC_Region_ID();
+		int bpRegionID = bpLoc != null ? bpLoc.getC_Location().getC_Region_ID() : 0;
+		
 		MLBRTaxDefinition[] taxesDef = MLBRTaxDefinition.get (oi.getAD_Org_ID(), bp.getC_BPartner_ID(), C_DocTypeTarget_ID, 
-				(oiLocation.getC_Location_ID() < 1 ? -1 : oiLocation.getC_Region_ID()), (bpLoc != null ? bpLoc.getC_Location().getC_Region_ID() : 0),
+				(isSOTrx ? orgRegionID : bpRegionID),
+				(isSOTrx ? bpRegionID : orgRegionID),
 				(isSOTrx ? bpW.getLBR_CustomerCategory_ID() : bpW.getLBR_VendorCategory_ID()), 
 				(isSOTrx ? bpW.getLBR_FiscalGroup_Customer_ID() : bpW.getLBR_FiscalGroup_Vendor_ID()), pW.getLBR_FiscalGroup_Product_ID(), 
 				pW.getLBR_NCM_ID(),  pW.getLBR_ProductCategory_ID(), p.getM_Product_ID() , hasSubstitution, isSOTrx, lbr_TransactionType, dateAcct);
