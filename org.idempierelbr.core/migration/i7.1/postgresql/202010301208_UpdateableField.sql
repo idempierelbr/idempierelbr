f9b99d8038bbbb7c6bee832219d9651c8047a4c5
alanlesc1 <alan.lesc1@gmail.com>
2020-10-30 08:14:44 -0700

    LBR_NFS_ID atualizável

---
+++ org.idempierelbr.core/migration/i7.1/postgresql/202010301208_UpdateableField.sql
@@ -0,0 +1,7 @@
+-- Set LBR_NFS_ID as updateable
+-- 30 de out de 2020 08:05:00 PDT
+UPDATE AD_Column SET IsUpdateable='Y',Updated=TO_TIMESTAMP('2020-10-30 08:05:00','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Column_ID=801144
+;
+
+SELECT lbr_register_migration_script('202010301208_UpdateableField.sql') FROM dual;
+
