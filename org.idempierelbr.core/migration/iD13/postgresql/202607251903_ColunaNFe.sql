99b23c38f8a4cb9c5031367c027a6bdb9e46d5a6
Alan Lescano <alan.lesc1@gmail.com>
2026-07-25 19:12:39 -0300

    Corrige typo

--- org.idempierelbr.core/migration/iD13/postgresql/202607251903_ColunaNFe.sql
+++ org.idempierelbr.core/migration/iD13/postgresql/202607251903_ColunaNFe.sql
@@ -1,5 +1,5 @@
 -- 
-SELECT register_migration_script('202607251903_ColunaNFe') FROM dual;
+SELECT register_migration_script('202607251903_ColunaNFe.sql') FROM dual;
 
 -- 25 de jul. de 2026 19:03:44 BRT
 INSERT INTO AD_Column (AD_Column_ID,Version,Name,Description,Help,AD_Table_ID,ColumnName,FieldLength,IsKey,IsParent,IsMandatory,IsTranslated,IsIdentifier,SeqNo,IsEncrypted,AD_Reference_ID,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,AD_Element_ID,IsUpdateable,IsSelectionColumn,EntityType,IsSyncDatabase,IsAlwaysUpdateable,IsAutocomplete,IsAllowLogging,AD_Column_UU,IsAllowCopy,SeqNoSelection,IsToolbarButton,IsSecure,FKConstraintType,IsHtml,IsPartitionKey) VALUES (802106,0,'Brazilian Fiscal Invoice (Nota Fiscal)','Brazilian Fiscal Invoice (Nota Fiscal) Identifier','The Brazilian Fiscal Invoice (Nota Fiscal) Document.',(SELECT AD_Table_ID FROM AD_Table WHERE AD_Table_UU = '5673b520-b8d0-4380-a24d-c35133ec7abe'),'LBR_NotaFiscal_ID',22,'N','N','N','N','N',0,'N',19,0,0,'Y',TO_TIMESTAMP('2026-07-25 19:03:43','YYYY-MM-DD HH24:MI:SS'),10,TO_TIMESTAMP('2026-07-25 19:03:43','YYYY-MM-DD HH24:MI:SS'),10,(SELECT AD_Element_ID FROM AD_Element WHERE AD_Element_UU = '9dcbc482-1130-41e7-ad99-152bb67e2db4'),'Y','N','LBR','N','N','N','Y','019f9b4e-3ed6-784d-a6a6-bee61dfcdb7d','Y',0,'N','N','C','N','N')
